4c355b23f4eae36a5bd8538c3446f3178ac57e8f
Poojitha Vadinuri <143255311+PoojithaVadinuri@users.noreply.github.com>
2023-09-24 18:05:37 +0530

    Update fa_tb.v

--- Day-1(Full_adder using half_adder)/fa_tb.v
+++ Day-1(Full_adder using half_adder)/fa_tb.v
@@ -1,16 +1,14 @@
 module tb;
-
 reg a,b,cin;
-
 wire s,cout;
 fa DUT(s,cout,a,b,cin);
 initial
   begin
-  repeat(5)
+  repeat(12)
     begin
     {a,b,cin}=$random;
 	#1;
-	$display(a,b,cin ,":",{cout,s});
+	$display("a=%b,b=%b,cin=%b,cout=%b,s=%b" ,a,b,cin,cout,s);
 	end
   end
 endmodule
